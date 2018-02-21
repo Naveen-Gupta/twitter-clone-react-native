e95fe54b9168f11acbe037a1dcfc381e398d9d39
Naveen Gupta <nvngupta1992@gmail.com>
2018-02-21 19:24:31 +0530

    added mutation for signup

---
+++ mobile/src/graphql/mutations/signup.js
@@ -0,0 +1,9 @@
+import { gql } from 'react-apollo';
+
+export default gql`
+    mutation signup($email: String!,$fullName: String!,$password: String!,$avatar: String,$username: String){
+        signup(email: $email, fullName: $fullName, password: $password, avatar: $avatar, username: $username){
+            token
+        }
+    }
+`;
